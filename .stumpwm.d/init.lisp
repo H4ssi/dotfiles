82d686298a3ee6abd57ede965db693064051093a
Florian Hassanen <florian.hassanen@gmail.com>
2017-07-17 22:49:56 +0200

    Provide some controls for screen brightness

--- .stumpwm.d/init.lisp
+++ .stumpwm.d/init.lisp
@@ -11,3 +11,27 @@
 
 (define-key *root-map* (kbd "c") "exec termite")
 (setf *mouse-focus-policy* :click)
+
+(defcommand brightness-get () ()
+  "Display current backlight brightness"
+  (echo (run-shell-command "light -G" t)))
+
+(defcommand brightness-up () ()
+  "Increase backlight brightness"
+  (run-shell-command "light -A 10")
+  (brightness-get))
+
+(defcommand brightness-down () ()
+  "Decrease backlight brightness"
+  (run-shell-command "light -U 10")
+  (brightness-get))
+
+(defcommand brightness-set (target)
+    ((:number "Brightness (%): "))
+  (run-shell-command (concatenate 'string "light -S " (write-to-string target)))
+  (brightness-get))
+
+(define-key *top-map* (kbd "XF86MonBrightnessUp") "brightness-up")
+(define-key *top-map* (kbd "XF86MonBrightnessDown") "brightness-down")
+(define-key *top-map* (kbd "C-XF86MonBrightnessUp") "brightness-set 100")
+(define-key *top-map* (kbd "C-XF86MonBrightnessDown") "brightness-set 1")
