bd123ddca58474addc84fa6617e0ab507fd55785
Florian Hassanen <florian.hassanen@gmail.com>
2017-07-20 19:38:21 +0200

    Do not restart swank-server once loaded

--- .stumpwm.d/init.lisp
+++ .stumpwm.d/init.lisp
@@ -1,10 +1,11 @@
 (in-package :stumpwm)
 
-(let ((matches (directory "~/.emacs.d/elpa/slime-*/swank-loader.lisp")))
-  (when matches
-    (load (first matches)) 
-    (funcall (find-symbol (string '#:init) (find-package :swank-loader)))
-    (funcall (find-symbol (string '#:create-server) (find-package :swank)))))
+(unless (find-package :swank-loader)
+  (let ((matches (directory "~/.emacs.d/elpa/slime-*/swank-loader.lisp")))
+    (when matches
+      (load (first matches))
+      (funcall (find-symbol (string '#:init) (find-package :swank-loader)))
+      (funcall (find-symbol (string '#:create-server) (find-package :swank))))))
 
 (run-shell-command "dex -ae XFCE")
 (run-shell-command "compton")
